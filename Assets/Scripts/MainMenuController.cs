90bd74ba1016997ca86b06b598055a9dbc874590
Murad Hajiyev <118837555+mhaciyev17623@users.noreply.github.com>
2026-05-02 14:49:09 +0400

    Add main menu navigation script

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Menu Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject instructionsPanel;
+    public GameObject creditsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowInstructions()
+    {
+        mainMenuPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
